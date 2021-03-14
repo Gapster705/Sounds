44d531b2ea3a21a2b500157da099d20a5b22d058
Gapster705 <80642049+Gapster705@users.noreply.github.com>
2021-03-14 21:12:03 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,5 @@
 basic.forever(function () {
+    music.setBuiltInSpeakerEnabled(false)
     if (input.buttonIsPressed(Button.A)) {
         music.playTone(330, music.beat(BeatFraction.Whole))
     } else if (input.buttonIsPressed(Button.B)) {
