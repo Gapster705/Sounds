6e1c2a64f7d513425024bf397470a33a89138b72
Gapster705 <80642049+Gapster705@users.noreply.github.com>
2021-03-14 21:08:11 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
 basic.forever(function () {
     if (input.buttonIsPressed(Button.A)) {
-        music.ringTone(262)
-    } else if (input.buttonIsPressed(Button.A)) {
-        music.ringTone(262)
+        music.playTone(330, music.beat(BeatFraction.Whole))
+    } else if (input.buttonIsPressed(Button.B)) {
+        music.playTone(440, music.beat(BeatFraction.Whole))
     } else {
         pins.digitalWritePin(DigitalPin.P0, 0)
     }
